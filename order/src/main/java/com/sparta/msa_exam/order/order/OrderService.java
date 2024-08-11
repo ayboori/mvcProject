9a05067d5ba85a10b2469899a5a6b5affea613dc
ayboori <105356296+ayboori@users.noreply.github.com>
2024-08-11 16:46:40 +0900

    에러 해결 중

--- order/src/main/java/com/sparta/msa_exam/order/order/OrderService.java
+++ order/src/main/java/com/sparta/msa_exam/order/order/OrderService.java
@@ -29,6 +29,11 @@ public class OrderService {
     }
 
     // 주문에 상품 추가
+    /*
+    1.  `FeignClient`를 이용해서 주문 서비스에 상품 서비스 클라이언트 연결
+    2. 상품 목록 조회 API를 호출해서 파라미터로 받은 `product_id` 가 상품 목록에 존재하는지 검증
+    3. 존재할경우 주문에 상품을 추가하고, 존재하지 않는다면 주문에 저장하지 않음
+     */
     @Transactional
     public OrderResponseDto addProduct(OrderProductRequestDto requestDto, Long orderId) {
 
@@ -51,10 +56,6 @@ public class OrderService {
 
         orderProductRepository.save(orderProduct);
         return new OrderResponseDto(order);
-    }
-
-    public String getProductInfo(String productId) {
-        return productClient.getProduct(productId);
     }
 
     public OrderResponseDto getOrder(Long orderId) {
